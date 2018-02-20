110a820c66852f748e192ab308685861b8df0d09
Chris Miller <csmiller4567@gmail.com>
2018-02-20 12:25:44 -0600

    update 2-20

--- guessing-game.js
+++ guessing-game.js
@@ -1,7 +1,7 @@
 // game logic
 
-function generateWinningNumber() {
-    return Math.floor(Math.random() * Math.floor(100)) + 1;
+function generateWinningNumber(bottom, top) {
+    return Math.floor(Math.random() * Math.floor(top - bottom + 1)) + bottom;
 }
 
 function shuffle(array) {
@@ -22,15 +22,17 @@ function shuffle(array) {
     return array;
 }
 
-function Game() {
+function Game(bottom, top) {
     this.playersGuess = null;
     this.pastGuesses = [];
-    this.winningNumber = generateWinningNumber();
+    this.winningNumber = generateWinningNumber(bottom, top);
+    this.hintBottom = null;
+    this.hintTop = null;
 }
 
 Game.prototype.playersGuessSubmission = function (num) {
     if ((num < 1 || num > 100 || typeof (num) !== 'number' || num !== num) && this.pastGuesses.length < 5) {
-        return 'That is an invalid guess.';
+        return 'Enter a valid guess.';
     }
     this.playersGuess = num;
     return this.checkGuess();
@@ -39,6 +41,7 @@ Game.prototype.playersGuessSubmission = function (num) {
 Game.prototype.checkGuess = function () {
     var pastGuess = this.pastGuesses.includes(this.playersGuess);
     var winningGuess = this.playersGuess == this.winningNumber
+    var magnitude = Math.abs(this.difference());
     if (!winningGuess && !pastGuess) {
         this.pastGuesses.push(this.playersGuess);
     }
@@ -51,27 +54,30 @@ Game.prototype.checkGuess = function () {
         if (pastGuess) {
             return 'You have already guessed that number.';
         }
-        if (this.difference() < 10) {
+        if (magnitude < 10) {
             return "You're burning up!"
         }
-        if (this.difference() < 25) {
+        if (magnitude < 25) {
             return "You're lukewarm."
         }
-        if (this.difference() < 50) {
+        if (magnitude < 50) {
             return "You're a bit chilly."
         }
-        if (this.difference() < 100) {
+        if (Mmagnitude < 100) {
             return "You're ice cold!"
         }
     }
 }
 
 Game.prototype.difference = function () {
-    return Math.abs(this.playersGuess - this.winningNumber);
+    return this.playersGuess - this.winningNumber;
+}
+
+Game.prototype.range = function () {
 }
 
 Game.prototype.direction = function () {
-    if (this.playersGuess === this.winningNumber) {
+    if (this.difference() === 0) {
         return this.winningNumber + '!';
     }
     if (this.playersGuess < this.winningNumber) {
@@ -82,45 +88,48 @@ Game.prototype.direction = function () {
 }
 
 Game.prototype.provideHint = function () {
+    // game.range();
     var hintArr = [];
     hintArr.push(this.winningNumber);
-    hintArr.push(generateWinningNumber());
-    hintArr.push(generateWinningNumber());
+    // hintArr.push(generateWinningNumber(this.hintBottom, this.hintTop));
+    // hintArr.push(generateWinningNumber(this.hintBottom, this.hintTop));
     return shuffle(hintArr);
 }
 
 function newGame() {
-    return new Game();
+    var bottom = 1;
+    var top = 100;
+    return new Game(bottom, top);
 }
 
 // game play (via JQuery)
 
 function submitGuess(game) {
-    var guessNum = +($('#player-input').val());
-    var result = game.playersGuessSubmission(guessNum);
+    var guess = +($('#player-input').val());
+    var result = game.playersGuessSubmission(guess);
     var turnNum = game.pastGuesses.length;
     $('#player-input').val('');
     $('#subtitle').text(result);
-    $('#guess-list li:nth-child(' + turnNum + ')').text(game.pastGuesses[turnNum - 1]);
     $('#tagline').text(game.direction());
+    $('#guess-list li:nth-child(' + turnNum + ')').text(game.pastGuesses[turnNum - 1]);
 }
 
 $(function () {
     var game = newGame();
-    $('#submit-button').click(function () {
+    $('#submit-button').click(function () { // submit guess on 'submit-button' click/enter
         submitGuess(game);
     })
-    $('#player-input').keyup(function (event) {
+    $('#player-input').keyup(function (event) { // submit guess on enter w/ 'player-input' focus
         if (event.which === 13) {
             submitGuess(game);
         }
     })
-
-    $('#reset').click(function () {
-        alert('reset');
+    $('#reset-btn').click(function () {
+        location.reload(true);
     })
-    $('#hint').click(function () {
-        alert('hint');
+    $('#hint-btn').click(function () {
+        var hint = game.provideHint();
+        $('#subtitle').text('The answer is ' + hint[0] + ', ' + hint[1] + ', or ' + hint[2] + '...');
     })
 })
 
